91197a5207f22e02a14353a5b90f2030916afaac
Pro-Sammo <ss.sammo2002@gmail.com>
2024-02-11 23:33:44 +0600

    fix login and logout layout

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
-import {  SignUp } from "@clerk/nextjs";
- 
+import { SignUp } from "@clerk/nextjs";
+
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+      <SignUp />
+    </div>
+  );
 }
